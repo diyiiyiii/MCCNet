364be3d0442d14f58a9e0084a22cfae217527857
kongxiuxiu <49397261+kongxiuxiu@users.noreply.github.com>
2021-02-03 13:09:10 +0800

    Update JTtest_video.py

--- JTtest_video.py
+++ JTtest_video.py
@@ -1,34 +1,26 @@
-import argparse
-from pathlib import Path
-import os
-
 import jittor as jt
 import jittor.nn as nn
 import jittor.transform as transforms
 from jittor.misc import save_image
-#from torchvision.utils import save_image
-
-from PIL import Image
-from os.path import basename
-from os.path import splitext
 
 from JTfunction import calc_mean_std, normal, coral
 import JTnet as net
 
-import jittor.models as jtmodels
-import torchvision.models as tcmodels
-
+import torch
 import numpy as np
 import cv2
 import time
-
-import torch
+import argparse
+import os
+from PIL import Image
+from os.path import basename
+from os.path import splitext
 
 jt.flags.use_cuda = 1 # jt.flags.use_cuda 表示是否使用 gpu 训练。
 # 如果 jt.flags.use_cuda=1，表示使用GPU训练 如果 jt.flags.use_cuda = 0 表示使用 CPU
 
-
-def test_transform(size, crop):   #测试图预处理
+#测试图预处理
+def test_transform(size, crop):
     transform_list = []
 
     if size != 0:
@@ -40,7 +32,8 @@ def test_transform(size, crop):   #测试图预处理
     transform = transforms.Compose(transform_list)
     return transform
 
-def style_transform(h, w):   #风格图预处理
+#风格图预处理
+def style_transform(h, w):
     k = (h, w)
     size = int(np.max(k))
     transform_list = []
@@ -50,18 +43,19 @@ def style_transform(h, w):   #风格图预处理
     transform = transforms.Compose(transform_list)
     return transform
 
-def content_transform():   #内容图预处理
+#内容图预处理
+def content_transform():
     transform_list = []
     transform_list.append(transforms.ToTensor())
     transform = transforms.Compose(transform_list)
     return transform
 
+#风格迁移
 def style_transfer(vgg, decoder, sa_module, content, style, alpha=1.0, interpolation_weights=None):
     assert (0.0 <= alpha <= 1.0)
 
     style_fs, content_f, style_f = feat_extractor(vgg, content, style)
     Fccc = sa_module(content_f, content_f)
-
 
     if interpolation_weights:
         _, C, H, W = Fccc.size()
@@ -74,11 +68,27 @@ def style_transfer(vgg, decoder, sa_module, content, style, alpha=1.0, interpola
         feat = sa_module(content_f, style_f)
 
     feat = feat * alpha + Fccc * (1 - alpha)
-    feat_norm = normal(feat)
+    #feat_norm = normal(feat)
     feat = feat
 
     return decoder(feat)
 
+#加载图片
+def get_files(img_dir):
+    files = os.listdir(img_dir)
+    paths = []
+    for x in files:
+        paths.append(os.path.join(img_dir, x))
+    return paths
+
+#加载参数文件
+def load_weight(model, path):
+    if '.pth' in path:
+        model.load_state_dict(torch.load(path))
+    else:
+        model.load_state_dict(jt.load(path))
+
+#获取feat
 def feat_extractor(vgg, content, style):
     enc_layers = list(vgg.children())
     enc_1 = nn.Sequential(*enc_layers[:4])  # input -> relu1_1
@@ -96,19 +106,40 @@ def feat_extractor(vgg, content, style):
 
     content_f = [Content3_1, Content4_1, Content5_1]
     style_f = [Style3_1, Style4_1, Style5_1]
-
-
     style_fs = [enc_1(style), enc_2(enc_1(style)), enc_3(enc_2(enc_1(style))), Style4_1, Style5_1]
 
     return style_fs, content_f, style_f
 
+#
+def image_process(content, style):
+    content_tf1 = content_transform()
+    content_frame = content_tf1(content)
+    content_frame = jt.array(content_frame)
+
+    h, w, c = np.shape(content_frame)
+    style_tf1 = style_transform(h, w)
+    style = style_tf1(style.convert("RGB"))
+    style = jt.array(style)
+
+    if preserve_color:
+        style = coral(style, content)
+
+    style = style.unsqueeze(0)
+    content = content_frame.unsqueeze(0)
+
+    with jt.no_grad():
+        output = style_transfer(vgg, decoder, mcc_module, content, style, alpha)
+    output = output.squeeze(0)
+
+    return output
+
 parser = argparse.ArgumentParser()
 # Basic options
-parser.add_argument('--content', type=str,
+parser.add_argument('--content', type=str,default="./content/blonde_girl.jpg",
                     help='File path to the content image')
 parser.add_argument('--content_dir', type=str,
                     help='Directory path to a batch of content images')
-parser.add_argument('--style', type=str,
+parser.add_argument('--style', type=str, default="./style/candy.jpg",
                     help='File path to the style image, or multiple style \
                     images separated by commas if you want to do style \
                     interpolation or spatial control')
@@ -116,38 +147,26 @@ parser.add_argument('--style_dir', type=str,
                     help='Directory path to a batch of style images')
 parser.add_argument('--output', type=str, default='output',
                     help='Directory to save the output image(s)')
-parser.add_argument('--decoder_path', type=str, default='./models/decoder_iter_160000.pkl')
-parser.add_argument('--transform_path', type=str, default='./models/mcc_module_iter_160000.pkl')
+parser.add_argument('--vgg_path', type=str, default='./experiments/vgg_normalised.pth')
+parser.add_argument('--decoder_path', type=str, default='./experiments/decoder_iter_160000.pth')
+parser.add_argument('--transform_path', type=str, default='./experiments/mcc_module_iter_160000.pth')
 parser.add_argument('--style_interpolation_weights', type=str, default="")
 parser.add_argument('--a', type=float, default=1.0)
-
 args = parser.parse_args()
-content="./content/blonde_girl.jpg"
-style="./style/candy.jpg"
-vgg_path='./experiments/vgg_normalised.pth'
-
 # Additional options
 content_size=512
 style_size=512
 crop='store_true'
 save_ext='.jpg'
 output_path=args.output
-
 # Advanced options
 preserve_color='store_true'
 alpha=args.a
 interpolation_weights = args.style_interpolation_weights
-
 preserve_color = False
 do_interpolation = False
 
-def get_files(img_dir):
-    files = os.listdir(img_dir)
-    paths = []
-    for x in files:
-        paths.append(os.path.join(img_dir, x))
-    return paths
-
+#加载测试数据，内容图+风格图
 if os.path.isdir(args.content_dir):
     content_paths = get_files(args.content_dir)
 else:  #单一图片
@@ -159,8 +178,6 @@ else:  #单一风格
 
 if not os.path.exists(output_path):
     os.mkdir(output_path)
-if not os.path.exists(output_path+"/content"):
-    os.mkdir(output_path+"/content")
 
 decoder = net.decoder
 vgg = net.vgg
@@ -171,18 +188,9 @@ decoder.eval()
 mcc_module.eval()
 vgg.eval()
 
-
-decoder.load_state_dict(jt.load(args.decoder_path))
-mcc_module.load_state_dict(jt.load(args.transform_path))  
-vgg.load_state_dict(torch.load(vgg_path))
-
-enc_layers = list(vgg.children())
-enc_layers = list(enc_layers[0].children())
-enc_1 = nn.Sequential(*enc_layers[:4])  # input -> relu1_1
-enc_2 = nn.Sequential(*enc_layers[4:11])  # relu1_1 -> relu2_1
-enc_3 = nn.Sequential(*enc_layers[11:18])  # relu2_1 -> relu3_1
-enc_4 = nn.Sequential(*enc_layers[18:31])  # relu3_1 -> relu4_1
-enc_5 = nn.Sequential(*enc_layers[31:44])  # relu4_1 -> relu5_1
+load_weight(decoder,args.decoder_path)
+load_weight(mcc_module,args.transform_path)
+load_weight(vgg,args.vgg_path)
 
 content_tf = test_transform(content_size, crop)
 style_tf = test_transform(style_size, crop)
@@ -190,9 +198,9 @@ style_tf = test_transform(style_size, crop)
 
 for content_path in content_paths:
     if do_interpolation:
-        style = jt.stack([style_tf1(Image.open(p)) for p in style_paths])
+        style = jt.stack([style_tf(Image.open(p)) for p in style_paths])
         #print(style.size())
-        content = content_tf1(Image.open(content_path)) \
+        content = content_tf(Image.open(content_path)) \
                   .unsqueeze(0).expand_as(style)
 
         with jt.no_grad():
@@ -209,15 +217,12 @@ for content_path in content_paths:
 
         if 'mp4' in content_path:
             for style_path in style_paths:
-                start = time.time()
-
                 video = cv2.VideoCapture(content_path)
                 j = 0
                 rate = video.get(5)
 
                 width = video.get(cv2.CAP_PROP_FRAME_WIDTH)
                 height = video.get(cv2.CAP_PROP_FRAME_HEIGHT) #获得帧宽和帧高
-                #print(width,height)
                 fps = int(rate)
 
                 video_name = outfile + '/{:s}_stylized_{:s}{:s}'.format(
@@ -227,76 +232,27 @@ for content_path in content_paths:
 
                 while(video.isOpened()):
                     j = j+1
-
                     ret, frame = video.read()
-
                     if ret == False :
                         break
 
                     if j%1 == False:
-                        content_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-                        content_tf1 = content_transform()
-                        content_frame = content_tf1(Image.fromarray(content_frame))
-                        content_frame = jt.array(content_frame)
-                        # save_image(content_frame, output_name1)
-
-                        h,w,c=np.shape(content_frame)
-                        style_tf1 = style_transform(h,w)
-                        style = style_tf1(Image.open(style_path).convert("RGB"))
-                        style = jt.array(style)
-
-                        if preserve_color:
-                            style = coral(style, content)
-
-                        style = style.unsqueeze(0)      #增加一个维度
-                        #np.savez('compare/cs', style=style.numpy())
-                        content = content_frame.unsqueeze(0)
-
-                        #np.savez('compare/cs',content=content.numpy(),style=style.numpy())
-
-
-                        with jt.no_grad():
-                            output = style_transfer(vgg, decoder, mcc_module, content, style, alpha)
-
-                        output = output.squeeze(0)
-                        #print(output.size())
+                        # 对每一帧进行风格化。
+                        style = Image.open(style_path)
+                        content = Image.fromarray(cv2.cvColor(frame, cv2.COLOR_BGR2RGB))
+                        output = image_process(frame,style)
+                        # 对风格化后的结果进行额外处理，以存储到视频中
                         output = output * 255 + 0.5
                         output = jt.uint8(jt.clamp(output,0,255).permute(1, 2, 0)).numpy()
-                        #print(output.shape)
-                        #output = output.mul_(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).type(torch.uint8).numpy()
-
                         output = cv2.cvtColor(output, cv2.COLOR_RGB2BGR)
-                        #print("保存视频的大小",output.shape)
-
                         videoWriter.write(output)  # 写入帧图
         else:
             for style_path in style_paths:
                 image_name = outfile +'/{:s}_stylized_{:s}{:s}'.format(
                         splitext(basename(content_path))[0],splitext(basename(style_path))[0], '.jpg')
 
-                content_tf1 = content_transform()
-                content_frame = content_tf1(Image.open(content_path))
-                content_frame = jt.array(content_frame)
-                # save_image(content_frame, output_name1)
-
-                h, w, c = np.shape(content_frame)
-                style_tf1 = style_transform(h, w)
-                style = style_tf1(Image.open(style_path).convert("RGB"))
-                style = jt.array(style)
-
-                if preserve_color:
-                    style = coral(style, content)
-
-                style = style.unsqueeze(0)  # 增加一个维度
-                # np.savez('compare/cs', style=style.numpy())
-                content = content_frame.unsqueeze(0)
-
-                # np.savez('compare/cs',content=content.numpy(),style=style.numpy())
-
-                with jt.no_grad():
-                    output = style_transfer(vgg, decoder, mcc_module, content, style, alpha)
-
-                output = output.squeeze(0)
+                content = Image.open(content_path)
+                style = Image.open(style_path)
+                output = image_process(content, style)
                 save_image(output, image_name)
 
